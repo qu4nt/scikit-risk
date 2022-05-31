34dbbd11c7800ca29e9844533ef433c26d34c6a0
Francisco «mapologo» Palm <fpalm@qu4nt.com>
2022-05-31 14:11:52 -0400

    Black formatted

--- setup.py
+++ setup.py
@@ -4,8 +4,7 @@ from setuptools import find_packages, setup
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
 
-description = ('probabilistic modeling, monte carlo',
-               'risk analysis')
+description = ("probabilistic modeling, monte carlo", "risk analysis")
 
 # The text of the README file
 README = (HERE / "README.md").read_text()
@@ -21,27 +20,28 @@ setup(
     author="qu4nt",
     author_email="team@qu4nt.com",
     license="GPLv3+",
-    classifiers=['Development Status :: 2 - Pre-Alpha,
-		     'Intended Audience :: End Users/Desktop',
-		     'Intended Audience :: Science/Research',
-		     'Intended Audience :: Information Technology',
-		     'Intended Audience :: Developers',
-		     'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-		     'Programming Language :: Python',
-		     'Programming Language :: Python :: 3',
-		     'Programming Language :: Python :: 3.7',
-		     'Programming Language :: Python :: 3.8',
-		     'Programming Language :: Python :: 3.9',
-		     'Programming Language :: Python :: 3.10',
-		     'Topic :: Office/Business',
-		     'Topic :: Scientific/Engineering', 
-		     'Topic :: Software Development',
-		     'Topic :: Software Development :: Libraries :: Python Modules',
-		     ],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Office/Business",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
     project_urls={
-        'Documentation': '',
-        'Funding': 'https://python.org/psf/',
-        'Source': '',
+        "Documentation": "",
+        "Funding": "https://python.org/psf/",
+        "Source": "",
     },
-    packages=find_packages()
+    packages=find_packages(),
 )
